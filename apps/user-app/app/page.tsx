09295a71ebac32c9573e7883706e281d4ea8cce8
Arup53 <arupkumarhowlader12@gmail.com>
2025-07-18 22:06:30 +0600

    fix name alias problem for db & store module

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,3 +1,8 @@
+"use client";
+
+import { useStore } from "@repo/store";
 export default function Home() {
+  const store = useStore((state) => state.balance);
+  console.log(store);
   return <div className="text-red-900">Hello, there</div>;
 }
