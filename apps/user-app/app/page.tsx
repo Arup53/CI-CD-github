5aeefdd8a4c6eee399734f1364b80585f60a63ad
Arup53 <arupkumarhowlader12@gmail.com>
2025-07-22 21:12:30 +0600

    Add workflows

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,8 +1,5 @@
 "use client";
 
-import { useStore } from "@repo/store";
 export default function Home() {
-  const store = useStore((state) => state.balance);
-  console.log(store);
   return <div className="text-red-900">Hello, there</div>;
 }
